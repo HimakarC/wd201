489c25e34286ca505eac46026e753b8b7a081680
HimakarC <116370824+HimakarC@users.noreply.github.com>
2023-12-04 20:55:06 +0530

    Add files via upload

---
+++ todo-cli/__tests__/todo.js
@@ -0,0 +1,78 @@
+const todoList = require("../todo");
+
+const { all, add, markAsComplete, overdue, dueToday, dueLater } = todoList();
+
+describe("Supriya the reason", () => {
+  beforeAll(() => {
+    let himakar = new Date();
+    add({
+      titile: "Himakar",
+      completed: false,
+      dueDate: new Date(new Date().setDate(himakar.getDate() + 1))
+        .toISOString()
+        .slice(0, 10),
+    });
+    add(
+    {
+      titile: "Bhavana",
+      completed: false,
+      dueDate: new Date(new Date().setDate(himakar.getDate() + 1))
+        .toISOString()
+        .slice(0, 10),
+    });
+    add(
+    {
+      titile: "Ananya",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    add(
+    {
+      titile: "Shivani",
+      completed: false,
+      dueDate: new Date(new Date().setDate(himakar.getDate() - 1))
+        .toISOString()
+        .slice(0, 10),
+    });
+    add(
+    {
+      titile: "Varshini",
+      completed: false,
+      dueDate: new Date(new Date().setDate(himakar.getDate() - 1))
+        .toISOString()
+        .slice(0, 10),
+    });
+  });
+  test("creating a new todo", () => {
+    let life = all.length;
+    add(
+    {
+      titile: "Rishitha",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    life += 1;
+    expect(all.length).toBe(life);
+  });
+
+  test("checks marking a todo as completed.", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+
+  test("checks retrieval of overdue items", () => {
+    const ananya = overdue();
+    expect(ananya.length).toBe(2);
+  });
+
+  test("checks retrieval of due today items", () => {
+    const runanya = dueToday();
+    expect(runanya.length).toBe(2);
+  });
+
+  test("checks retrieval of due later items", () => {
+    const aranya = dueLater();
+    expect(aranya.length).toBe(2);
+  });
+});
